f35c6616706ba4a66ddbabd6102bddcdc17ad618
Samrattudugani <tuduganisamrat@gmail.com>
2026-02-06 14:35:24 +0530

    Initial commit

---
+++ IV-SEM/Python/Practice/M02_Logic_Building_and_Patterns/S01/PS01_Digit_Problems.py
@@ -0,0 +1,53 @@
+### 1.COUNT THE dS ######
+# n = int(input())
+# c = 0
+# while(n > 0):
+#     c+=1
+#     n = n // 10
+# print(c) 
+#### 2.sum of the d ####
+# n = int(input())
+# c = 0
+# while n > 0:
+#     d = n % 10
+#     c += d
+#     n = n // 10
+# print(c)
+##### 3.Product OF dS ######
+# n = int(input())
+# c = 1
+# while n > 0:
+#     d = n % 10
+#     c *= d
+#     n = n // 10
+# print(c)
+##### 4.REVERSE THE dS #######
+# n = int(input())
+# r = 0
+# while(n > 0):
+#     r = r*10 + n % 10
+#     n//=10
+# print(r)
+#### 5.COUNT ODD AND EVEN dS ######################
+# n = input()
+# o = 0
+# e = 0
+# for i in n:
+#     if int(i) % 2 == 0:
+#         e+=1
+#     else:
+#         o += 1
+# print(e,o)
+#### 6.Print Largl number in the d #######
+# n = input()
+# print(max(n))
+# ##########
+# n = int(input())
+# l = 0
+# while n > 0:
+#     d = n % 10
+#     if d > l:
+#         l = d
+#     n //= 10
+# print(l)
+
