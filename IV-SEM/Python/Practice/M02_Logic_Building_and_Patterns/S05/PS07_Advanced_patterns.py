6f129b847a209c2a03748572dba7020fd163b74b
Samrattudugani <tuduganisamrat@gmail.com>
2026-03-12 22:27:58 +0530

    Added task solutions

--- IV-SEM/Python/Practice/M02_Logic_Building_and_Patterns/S05/PS07_Advanced_patterns.py
+++ IV-SEM/Python/Practice/M02_Logic_Building_and_Patterns/S05/PS07_Advanced_patterns.py
@@ -1,25 +1,203 @@
-"""
-Time complexity : it depends on input //size ^ tc ^
-Defination : it is measured based on input.
-O(1) - constant Time 
-ex: n = 10
-    print(n)
-O(n) - single loop
-O(n^2) - Two loops 
-O(Log n) - Binary search
-O(n Log n) - linearthimic 
-O(2^n) - Recursion 
 
+# 1. Pascal Triangle
+# n=5
+# Output:
+# 1
+# 1 1
+# 1 2 1
+# 1 3 3 1
+# 1 4 6 4 1
+# n = int(input())
+# for i in range(n):
+#     num = 1
+#     for j in range(i + 1):
+#         print(num, end=" ")
+#         num = num * (i - j) // (j+1)
+#     print()
+# 2. Butterfly Pattern
+# n=4
+# Output:
+# *      *
+# **    **
+# ***  ***
+# ********
+# ********
+# ***  ***
+# **    **
+# *      *
+# n = int(input())
+# for i in range(1, n + 1):
+#     print("*" * i + " " * (2 * (n - i)) + "*" * i)
+# for i in range(n, 0, -1):
+#     print("*" * i + " " * (2 * (n - i)) + "*" * i)
+# 3. Hourglass Pattern
+# n=4
+# Output:
+# * * * *
+# * * *
+# * *
+# *
+# * *
+# * * *
+# * * * *
+n = int(input())
 
-print("TIME COMPLEXITY") # O(1)
-n = [1,2,3,4,5]
-for  i in n:
-    print(i) # O(n)
+# Upper half
+for i in range(n, 0, -1):
+    print("* " * i)
 
-n = [1,2,3,4,5]
-for  i in n:
-    for j in n:
-        print(i,j) #O(n^2)
+# Lower half
+for i in range(2, n + 1):
+    print("* " * i)
+# 4. Concentric Square
+# n=3
+# Output:
+# 3 3 3 3 3
+# 3 2 2 2 3
+# 3 2 1 2 3
+# 3 2 2 2 3
+# 3 3 3 3 3
+n = int(input())
+size = 2 * n - 1
 
-"""
-Linear search : 
+for i in range(size):
+    for j in range(size):
+        value = n - min(i, j, size - i - 1, size - j - 1)
+        print(value, end=" ")
+    print()
+# 5. Snake Pattern
+# n=4
+# Output:
+# 1 2 3 4
+# 8 7 6 5
+# 9 10 11 12
+# 16 15 14 13
+n = int(input())
+num = 1
+
+for i in range(n):
+    row = []
+    for j in range(n):
+        row.append(num)
+        num += 1
+    
+    if i % 2 != 0:
+        row.reverse()
+    
+    print(*row)
+
+# 6. Spiral Matrix
+# n=3
+# Output:
+# [1, 2, 3]
+# [8, 9, 4]
+# [7, 6, 5]
+n = int(input())
+matrix = [[0]*n for _ in range(n)]
+
+top, bottom = 0, n-1
+left, right = 0, n-1
+num = 1
+
+while top <= bottom and left <= right:
+    
+    for i in range(left, right+1):
+        matrix[top][i] = num
+        num += 1
+    top += 1
+    
+    for i in range(top, bottom+1):
+        matrix[i][right] = num
+        num += 1
+    right -= 1
+    
+    for i in range(right, left-1, -1):
+        matrix[bottom][i] = num
+        num += 1
+    bottom -= 1
+    
+    for i in range(bottom, top-1, -1):
+        matrix[i][left] = num
+        num += 1
+    left += 1
+
+for row in matrix:
+    print(row)
+
+# 7. Number Spiral
+# n=5
+# Output:
+# [1, 2, 3, 4, 5]
+# [16,17,18,19,6]
+# [15,24,25,20,7]
+# [14,23,22,21,8]
+# [13,12,11,10,9]
+n = int(input())
+matrix = [[0]*n for _ in range(n)]
+
+top, bottom = 0, n-1
+left, right = 0, n-1
+num = 1
+
+while top <= bottom and left <= right:
+    
+    for i in range(left, right+1):
+        matrix[top][i] = num
+        num += 1
+    top += 1
+    
+    for i in range(top, bottom+1):
+        matrix[i][right] = num
+        num += 1
+    right -= 1
+    
+    for i in range(right, left-1, -1):
+        matrix[bottom][i] = num
+        num += 1
+    bottom -= 1
+    
+    for i in range(bottom, top-1, -1):
+        matrix[i][left] = num
+        num += 1
+    left += 1
+
+for row in matrix:
+    print(row)
+
+# 8. X Pattern Numbers
+# n=5
+# Output:
+# 1       1
+#  2   2
+#    3
+#  4   4
+# 5       5
+n = int(input())
+
+for i in range(n):
+    for j in range(n):
+        if j == i or j == n - i - 1:
+            print(i + 1, end="")
+        else:
+            print(" ", end="")
+    print()
+
+# 9. Hollow Butterfly
+# n=4
+# Output:
+# *      *
+# **    **
+# ***  ***
+# ********
+# ***  ***
+# **    **
+# *      *     
+n = int(input())
+
+# Upper half
+for i in range(1, n+1):
+    print("*" * i + " " * (2*(n-i)) + "*" * i)
+
+# Lower half
+for i in range(n-1, 0, -1):
+    print("*" * i + " " * (2*(n-i)) + "*" * i)
---
+++ IV-SEM/Python/Practice/M03_Core_Concepts/S02/PS02_Python_Collections.py
@@ -0,0 +1,30 @@
+# # 1.Creating of list 
+# a = [1,23,45,68]
+# print(a)
+# d = list((1,23,4,5,6,7,98))
+# print(d)
+# # 2.ACCESSING OF LIST : INDEX 0 , -1
+# k = [1,2,3,4,5]
+# print(k[1])
+# print(k[-1])
+# print(k[2])
+# # 3.CREATING LIST WITH REPEATED ELEMENTS
+# s = [1,2,3,4]
+# print(s*2)
+# #4. ADDING ELEMENTS TO A LIST 
+# A = [1,2,3,4]
+# A.append(50)
+# A.insert(1,30)
+# print(A)
+# # 5. Removing Elements from List
+# B = list((1,2,3,4,5,6,7))
+# print(B)
+# B.remove(7)
+# print(B)
+# B.pop()
+# print(B)
+# B.clear()
+# print(B)
+##############################################################33
+############### MAJORITY ELEMENT #######################################
+
